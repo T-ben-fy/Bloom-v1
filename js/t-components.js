f12ae6d6f18b29121425dc7f3ae19fc4533b1ff8
T-ben-fy <tarik.benkiran.fycomputing@gmail.com>
2025-04-03 11:40:04 +0000

    Update t-components.js

--- js/t-components.js
+++ js/t-components.js
@@ -1,5 +1,5 @@
 'use strict';
-xxx
+
 let main_subclasses       = ['btn','backlog','tag','select','collapse','checkbox','col'];
 let main_subclasses__vars = [
     'primary','secondary','success','orange','danger','','primary','primary',
