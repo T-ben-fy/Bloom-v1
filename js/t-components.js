a1019ce8d7d46c9fe09faab48a113b62e1b555dd
tarik.benkiran.fycomputing@gmail.com <tarik.benkiran.fycomputing@gmail.com>
2025-04-03 09:59:42 +0000

    Bloom v1 ( 4 / 3 / 25)

---
+++ js/t-components.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let main_subclasses       = ['btn','backlog','tag','select','collapse','checkbox','col'];
+let main_subclasses__vars = [
+    'primary','secondary','success','orange','danger','','primary','primary',
+    'backlog',
+    'tag',
+    'select',
+    'collapse',
+    'checkbox',
+    'col'
+];
+
+
+let bl_components = [
+    {
+        id : 0,
+        name : '',
+        class__name : 'bl-btn',
+        html__code  : 
+                    `<div class='btn'>
+                        <span>Bloom Button</span>
+                    </div>`
+    },
+    {
+        id : 0,
+        name : '',
+        class__name : 'bl-btn',
+        html__code  : 
+                    `<div class='btn'>
+                        <span>Bloom Button</span>
+                    </div>`
+    },
+    {
+        id : 0,
+        name : '',
+        class__name : 'bl-btn',
+        html__code  : 
+                    `<div class='btn'>
+                        <span>Bloom Button</span>
+                    </div>`
+    },
+];
